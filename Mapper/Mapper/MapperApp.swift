c6323f023e4454d29d8913a01626f34ef92f6ffb
NicholasPilotto <nicholaspilotto@gmail.com>
2023-08-18 10:29:37 +0200

    Project first commit

---
+++ Mapper/Mapper/MapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapperApp.swift
+//  Mapper
+//
+//  Created by Nicholas Pilotto on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
