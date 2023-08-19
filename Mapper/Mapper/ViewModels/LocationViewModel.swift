a7d3ad629c1ce89160c54c93c9ebb83be7dc141e
NicholasPilotto <nicholaspilotto@gmail.com>
2023-08-19 15:06:13 +0200

    Show locations list

--- Mapper/Mapper/ViewModels/LocationViewModel.swift
+++ Mapper/Mapper/ViewModels/LocationViewModel.swift
@@ -18,9 +18,12 @@ class LocationViewModel: ObservableObject {
       updateMapRegion(location: mapLocation)
     }
   }
-  
+  // current region on map
   @Published var mapRegion = MKCoordinateRegion()
   let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+  
+  // show list of locations
+  @Published var showLocationsList = false
   
   init() {
     let locations = LocationsDataService.locations
@@ -34,6 +37,19 @@ class LocationViewModel: ObservableObject {
   private func updateMapRegion(location: Location) {
     withAnimation(.easeOut) {
       mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+    }
+  }
+  
+  public func locationsListToggle() {
+    withAnimation(.easeInOut) {
+      showLocationsList.toggle()
+    }
+  }
+  
+  public func showNextLocation(location: Location) {
+    withAnimation(.easeInOut) {
+      self.mapLocation = location
+      showLocationsList = false
     }
   }
 }
